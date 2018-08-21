758c1ee83b367166ebf53ec622c83e01b37a8722
Vedika Parvez <vedikaparvez@hotmail.com>
2018-08-21 12:08:44 +0530

    Remove keys

--- twitter-sentiment-analysis-python.py
+++ twitter-sentiment-analysis-python.py
@@ -2,11 +2,11 @@ import tweepy
 from textblob import TextBlob
 import csv
 
-consumer_key = "t0U0lP5o50wOeJpa2fyzfkzZW"
-consumer_secret = "Zl6o92NvY3cSAvPbiN93Nv2Z8yvfKNio1kxT1knmeLESjRY3Zp"
+consumer_key = ""
+consumer_secret = ""
 
-access_token = "1024154522258558976-HnKsG6mmFpga0GX6688JYCSxumAfle"
-access_token_secret = "1ksopwEp1gHL6oIETrGWP5pVZwJBgW8u1Xddwe7Ky6VGG"
+access_token = ""
+access_token_secret = ""
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
